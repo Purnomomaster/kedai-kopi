398f35b0e9639bc182b8ffcada5e78aa22eb0782
Purnomomaster <pari.purnomo@gmail.com>
2024-04-12 14:46:28 +0700

    create anim branch

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ document.addEventListener('click', function (e) {
         nav.classList.remove('active');
     }
 })
+// start anim
+const handleMouseMove = e => {
+    const { currentTarget: target } = e
+    const rect = target.getBoundingClientRect(), x = e.clientX - rect.left, y = e.clientY - rect.top
+    target.style.setProperty("--mouse-x", `${x}px`)
+    target.style.setProperty("--mouse-y", `${y}px`)
+}
+for (const card of document.querySelectorAll(".menu-card")) {
+    card.onmousemove = e => handleMouseMove(e)
+}
